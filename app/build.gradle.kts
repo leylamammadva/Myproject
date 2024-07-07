dbab6135fe3229bdfb00f0e15ff71a0665072b2c
leylamammadva <leyla.mammadova018@gmail.com>
2024-07-08 02:04:08 +0400

    Homework 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,16 +34,10 @@ android {
         jvmTarget = "1.8"
     }
 }
-
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.filament.android)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
 }
+
